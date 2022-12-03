5b3a8dab9f708c41789c4f61a05638d611815a5e
lijunlin <lijunlin2022@gmail.com>
2022-12-04 01:15:28 +0800

    feat: add navbar and sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,8 +1,30 @@
+const { getSidebarItems } = require('./getSidebarItems')
+
 export default {
   title: `LiJunlin's Blog`,
   titleTemplate: false,
   themeConfig: {
     siteTitle: `LiJunlin's Blog`,
-    logo: '/clover.png'
+    logo: '/clover.png',
+    nav: [
+      {
+        text: 'Blog',
+        link: '/blog/basic-knowledge-of-javascript/'
+      },
+      {
+        text: 'Meditation',
+        link: '/meditation/'
+      }
+    ],
+    sidebar: {
+      '/blog/basic-knowledge-of-javascript/': [{
+        text: 'JavaScript 基础知识',
+        items: getSidebarItems('../blog/basic-knowledge-of-javascript/')
+      }],
+      '/meditation/': [{
+        text: '英文随笔',
+        items: getSidebarItems('../meditation/')
+      }]
+    }
   }
 }
---
+++ docs/.vitepress/getSidebarItems.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+
+const getSidebarItems = (relativePath) => {
+  const dirPath = path.join(__dirname, relativePath)
+  const fileNames = fs.readdirSync(dirPath).filter(v => v !== 'index.md')
+  const items = []
+  fileNames.forEach(name => {
+    const filePath = dirPath + name
+    const firstLine = fs.readFileSync(filePath).toString('utf-8').split('\n')[0]
+    const text = firstLine.split('#')[1].trim()
+    const link = relativePath.split('.').pop() + name
+    items.push({
+      text,
+      link
+    })
+  })
+  return items
+}
+
+module.exports = {
+  getSidebarItems
+}
