4af46b1977a0307efa873fb6fc43c87abdb2c17d
lijunlin <lijunlin2022@gmail.com>
2023-07-25 01:58:15 +0800

    fix: 引入 BrowserOnly 组件, 解决打包失败的问题

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import styles from './index.module.css'
 import ParallaxHeader from '../components/ParallaxHeader';
 import ParallaxFooter from '../components/ParallaxFooter';
 import Parallax from '../components/Parallax';
-import { isMobile } from '../utils/env';
 
 export default function Home() {
   return (
-    <Layout>
-      {
-        isMobile ? (
-          <div className={styles.mobileContainer}>
-          </div>
-        ) : (
-          <div className={styles.container}>
-            <ParallaxHeader />
-            <Parallax />
-            <Parallax hasText={true} />
-            <Parallax hasText={true} />
-            <ParallaxFooter />
-          </div>
-        )
-      }
-    </Layout>
+      <Layout>
+        <BrowserOnly>
+          {() => {
+            if (/Mobi|Android|iPhone/i.test(window.navigator.userAgent)) {
+              return (
+                <div className={styles.mobileContainer}></div>
+              )
+            } else {
+              return (
+                <div className={styles.container}>
+                  <ParallaxHeader />
+                  <Parallax />
+                  <Parallax hasText={true} />
+                  <Parallax hasText={true} />
+                  <ParallaxFooter />
+                </div>
+              )
+            }
+          }}
+        </BrowserOnly>
+      </Layout>
   );
 }
--- src/utils/env.js
+++
@@ -1,5 +0,0 @@
-const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
-
-export {
-  isMobile,
-};
