b902d7a7fb6031d41ce6d3c44b709d47e05908f8
lijunlin <lijunlin2022@gmail.com>
2023-11-05 10:09:58 +0800

    feat: 更新主页

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,11 @@ import Code from "../components/Code/index";
 const code = `
 function package (bagWeight, value, weight) {
   let result = [];
+
   for (let j = 0; j <= bagWeight; j++) {
     result[j] = j >= weight[0] ? value[0] : 0;
   }
+
   for (let i = 1; i < value.length; i++) {
     const next = [];
     for (let j = 0; j <= bagWeight; j++) {
@@ -19,6 +21,7 @@ function package (bagWeight, value, weight) {
     }
     result = next;
   }
+
   return result[bagWeight];
 }
 
@@ -76,6 +79,72 @@ export default function Home() {
     },
   ];
 
+  const texts2 = [
+    {
+      content: "I enjoy ",
+      color: "text-white",
+    },
+    {
+      content: "city walks",
+      color: "custom-text-yellow",
+    },
+    {
+      content: " , ",
+      color: "text-white",
+    },
+    {
+      content: "writing",
+      color: "custom-text-red",
+    },
+    {
+      content: " and the ",
+      color: "text-white",
+    },
+    {
+      content: "occasional trip",
+      color: "custom-text-green",
+    },
+    {
+      content: ". I think that ",
+      color: "text-white",
+    },
+    {
+      content: "a happy life is colorful",
+      color: "custom-text-yellow",
+    },
+    {
+      content: ".",
+      color: "text-white",
+    },
+  ];
+
+  const texts3 = [
+    {
+      content: "Of course, people can't do ",
+      color: "text-white",
+    },
+    {
+      content: "whatever they want in the world",
+      color: "custom-text-red",
+    },
+    {
+      content: " , ",
+      color: "text-white",
+    },
+    {
+      content: "so I have been looking for the art of ",
+      color: "text-white",
+    },
+    {
+      content: "compromise with life",
+      color: "custom-text-green",
+    },
+    {
+      content: " .",
+      color: "text-white",
+    },
+  ];
+
   const btns = [
     {
       name: "Docs",
@@ -116,7 +185,7 @@ export default function Home() {
           ))}
         </div>
       </div>
-      <div className="flex flex-col justify-between items-center my-10 lg:flex-row">
+      <div className="flex flex-col justify-between items-center my-10 lg:flex-row lg:my-40 lg:justify-start">
         <div className="max-w-xl">
           {texts.map(({ content, color }) => (
             <span
@@ -127,15 +196,12 @@ export default function Home() {
             </span>
           ))}
         </div>
-        <div className="hidden custom-bg-black lg:block">
-          <Code code={code} className="custom-bg-black px-0" />
-        </div>
       </div>
       <div className="flex flex-col justify-center lg:flex-row">
         {btns.map(({ name, to }) => (
           <div
             key={name}
-            className="flex justify-center mx-3 px-10 py-6 mb-3 bg-gray-600 rounded-2xl hover:bg-gray-500 cursor-pointer"
+            className="flex justify-center mx-3 px-10 py-6 mb-3 bg-[#5e81ac] rounded-2xl hover:bg-[#81a1c1] cursor-pointer"
           >
             <Link
               to={to}
@@ -145,6 +211,33 @@ export default function Home() {
             </Link>
           </div>
         ))}
+      </div>
+      <div className="flex flex-col justify-between items-center my-10 lg:flex-row lg:my-40 lg:justify-end">
+        <div className="max-w-xl">
+          {texts2.map(({ content, color }) => (
+            <span
+              key={content}
+              className={`text-3xl leading-normal font-tw font-semibold ${color} lg:text-4xl lg:leading-relaxed`}
+            >
+              {content}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col justify-between items-center my-10 lg:flex-row lg:my-40 lg:justify-start">
+        <div className="max-w-4xl">
+          {texts3.map(({ content, color }) => (
+            <span
+              key={content}
+              className={`text-3xl leading-normal font-tw font-semibold ${color} lg:text-4xl lg:leading-relaxed`}
+            >
+              {content}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className="hidden bg-[#272b33] lg:block lg:max-w-4xl lg:my-40">
+        <Code code={code} className="p-10 text-base bg-[#272b33]" />
       </div>
     </main>
   );
