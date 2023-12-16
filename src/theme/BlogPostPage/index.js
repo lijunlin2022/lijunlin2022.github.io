508e7982a39cbb25fc6376689300636f6f9ac671
lijunlin <lijunlin2022@gmail.com>
2023-12-16 17:10:48 +0800

    feat: 接入评论

--- src/theme/BlogPostPage/index.js
+++ src/theme/BlogPostPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import {HtmlClassNameProvider, ThemeClassNames} from '@docusaurus/theme-common';
 import {BlogPostProvider, useBlogPost} from '@docusaurus/theme-common/internal';
@@ -10,12 +10,30 @@ import TOC from '@theme/TOC';
 import Unlisted from '@theme/Unlisted';
 function BlogPostPageContent({sidebar, children}) {
   const {metadata, toc} = useBlogPost();
+  const commentElement = useRef(null);
   const {nextItem, prevItem, frontMatter, unlisted} = metadata;
   const {
     hide_table_of_contents: hideTableOfContents,
     toc_min_heading_level: tocMinHeadingLevel,
     toc_max_heading_level: tocMaxHeadingLevel,
   } = frontMatter;
+
+  useEffect(() => {
+    const blogIndex = window.location.href.indexOf('blog');
+    const theme = document.documentElement.getAttribute('data-theme');
+    if (commentElement.current && blogIndex > 0) {
+      const issueTerm = window.location.href.slice(blogIndex);
+      let s = document.createElement("script");
+      s.src = "https://utteranc.es/client.js";
+      s.setAttribute("repo", "lijunlin2022/blog-issues");
+      s.setAttribute("theme", theme === 'light' ? 'github-light' : 'github-dark-orange');
+      s.setAttribute("issue-term", issueTerm);
+      s.setAttribute("crossorigin", "anonymous");
+      s.async = true;
+      commentElement.current.appendChild(s);
+    }
+  }, [commentElement]);
+
   return (
     <BlogLayout
       sidebar={sidebar}
@@ -34,6 +52,8 @@ function BlogPostPageContent({sidebar, children}) {
       {(nextItem || prevItem) && (
         <BlogPostPaginator nextItem={nextItem} prevItem={prevItem} />
       )}
+
+      <div ref={commentElement}></div>
     </BlogLayout>
   );
 }
