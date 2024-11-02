ad817d0009da4b3f2ec3614f9859e0280410e6cc
lijunlin <lijunlin2022@gmail.com>
2024-11-02 22:34:18 +0800

    [blog] 按照月份重构博客目录

---
+++ docs/blog/2024/11/02/index.md
@@ -0,0 +1,19 @@
+# H5 套 H5 叫微前端，那么小程序套小程序呢？
+
+## 打开另一个小程序
+
+[打开另一个小程序 | 微信官方文档](https://developers.weixin.qq.com/miniprogram/dev/api/navigate/wx.navigateToMiniProgram.html)
+
+![](./img/miniprogram.png)
+
+## 打开半屏小程序
+
+[打开半屏小程序 | 微信官方文档](https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/openEmbeddedMiniProgram.html)
+
+![](./img/embedded-miniprogram.png)
+
+## 打开子包
+
+![](./img/sub-miniprogram-pdd.png)
+
+![](./img/sub-mimiprogram-jd.png)
--- rspress.config.ts
+++ rspress.config.ts
@@ -1,29 +1,4 @@
 import { defineConfig } from 'rspress/config';
-import type { RspressPlugin } from '@rspress/shared';
-
-const blogPattern = /^\/blog\/(\d{4})-(\d{2})-(\d{2})-([a-zA-Z0-9_-]+)\/$/
-
-function transformRoute(): RspressPlugin {
-  return {
-    // 插件名称
-    name: 'plugin-routes',
-    // 在构建之后执行的钩子
-    async routeGenerated(routes, isProd) {
-      routes.forEach((r, index) => {
-        const match = r.routePath.match(blogPattern)
-        console.log(r.routePath)
-        if (match) {
-          const [, year, month, day, remainingPart] = match
-          const val =  `/blog/${year}/${month}/${day}/${remainingPart}`
-          routes.push({
-            ...routes[index],
-            routePath: val
-          })
-        }
-      })
-    },
-  };
-}
 
 export default defineConfig({
   title: '小霖家的混江龙',
@@ -34,5 +9,4 @@ export default defineConfig({
   route: {
     cleanUrls: true,
   },
-  plugins: [transformRoute()]
 });
