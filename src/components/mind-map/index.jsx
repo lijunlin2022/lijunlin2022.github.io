b4c1468220f8865e266862e60273ad12b0418491
lijunlin <lijunlin2022@gmail.com>
2024-11-28 00:23:53 +0800

    [src/components] 新增思维导图组件

---
+++ src/components/mind-map/index.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Markmap } from "markmap-view";
+import { transformer } from "./markmap";
+import { Toolbar } from "markmap-toolbar";
+import "markmap-toolbar/dist/style.css";
+
+const defaultStyle = {
+  minHeight: "50vh",
+  backgroundColor: "#f1f1f1",
+};
+
+const FULL_SVG = "M16 4h-12v12h12v-8h-8v4h2v-2h4v4h-8v-8h10z";
+
+export default function MindMap({ markdown }) {
+  const [value] = useState(markdown);
+  const [style, setStyle] = useState(defaultStyle);
+
+  const refSvg = useRef();
+  const refMm = useRef();
+  const refToolbar = useRef();
+
+  const renderToolbar = (mm, wrapper) => {
+    while (wrapper?.firstChild) {
+      wrapper.firstChild.remove();
+    }
+
+    if (mm && wrapper) {
+      const toolbar = new Toolbar();
+      toolbar.attach(mm);
+
+      toolbar.register({
+        id: "full",
+        title: "full screen",
+        content: Toolbar.icon(FULL_SVG),
+        onClick: () => {
+          if (refSvg.current.style.position === "fixed") {
+            setStyle(defaultStyle);
+            return;
+          }
+          setStyle({
+            position: "fixed",
+            right: 0,
+            bottom: 0,
+            width: "100vw",
+            height: "calc(100vh - var(--rp-nav-height))",
+            backgroundColor: "#f1f1f1",
+          });
+        },
+      });
+
+      toolbar.setItems(["zoomIn", "zoomOut", "fit", "full"]);
+      wrapper.append(toolbar.render());
+    }
+  };
+
+  useEffect(() => {
+    if (refMm.current) {
+      return;
+    }
+    const mm = Markmap.create(refSvg.current);
+    refMm.current = mm;
+    renderToolbar(refMm.current, refToolbar.current);
+  }, [refSvg.current]);
+
+  useEffect(() => {
+    const mm = refMm.current;
+    if (!mm) {
+      return;
+    }
+    const { root } = transformer.transform(value);
+    mm.setData(root);
+    mm.fit();
+  }, [refMm.current, value]);
+
+  useEffect(() => {
+    const mm = refMm.current;
+    if (!mm) {
+      return;
+    }
+    mm.fit();
+  }, [style]);
+
+  return (
+    <React.Fragment>
+      <div className="relative" style={style}>
+        <svg className="w-full h-full" ref={refSvg} style={style}></svg>
+        <div className="absolute bottom-0 right-0" ref={refToolbar}></div>
+      </div>
+    </React.Fragment>
+  );
+}
